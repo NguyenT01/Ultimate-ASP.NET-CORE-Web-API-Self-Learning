84da5f60dbce42550896cf74d881c705d7d56298
Nguyen Tran <transinguyen2001@gmail.com>
2024-01-05 15:30:11 +0700

    Chapter 01: Complete

---
+++ ch1/CompanyEmployee/CompanyEmployee/Extensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace CompanyEmployee.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder.AllowAnyOrigin();
+                    builder.AllowAnyHeader();
+                    builder.AllowAnyMethod();
+                });
+            });
+        }
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+    }
+}
---
+++ ch1/CompanyEmployee/CompanyEmployee/Program.cs
@@ -0,0 +1,33 @@
+using CompanyEmployee.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseCors("CorsPolicy");
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
